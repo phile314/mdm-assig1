2f85ab81c3510529d6934a04ef79511e3315186a
Philipp Hausmann <ph_git@314.ch>
2014-09-11 18:38:00 +0200

    Seperate data reading into a function

--- src/main.r
+++ src/main.r
@@ -1,7 +1,7 @@
 
+source("common.r")
 
-indians.dat <- read.csv("../data/pima.txt")
-indians.dat.xs <- indians.dat[,1:8]
-indians.dat.ys <- indians.dat[,9]
+pima <- read_data("../data/pima.txt")
+
 
 
