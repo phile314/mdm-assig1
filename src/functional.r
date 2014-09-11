ca651d7c43904afda37cde588b32fb8e869c9a4b
Philipp Hausmann <ph_git@314.ch>
2014-09-11 17:13:31 +0200

    Add main

--- src/functional.r
+++ src/functional.r
@@ -1,6 +1,10 @@
 source("common.r")
 
-tree.functional.classify <- function(xs) UseMethod("tree.functional.classify")
+tree.functional.classifyI <- function(tr,x) UseMethod("tree.functional.classifyI")
+
+tree.functional.classify <- function(tr,xs) {
+    apply(xs, 1, (tree.functional.classifyI tr)
+}
 
 tree.functional.grow <- function(xs, ys) {
     maj <- majority_class ys
@@ -11,15 +15,30 @@ tree.functional.grow <- function(xs, ys) {
 # either split the given rows, or create a leaf node
 tree.functional.growI <- function(xs, ys, cls) {
     sp <- get_best_split(xs, ys)
-    if () {
+    if (sp .... nil) {
         return(mkLeaf(xs, ys, cls))
     } else {
-        return(mkNode(....))
+        # split data
+        
+
+        l <- tree.functional.growI(,, 0)
+        r <- tree.functional.growI(,, 1)
+        return(mkNode(l, r,..))
     }
 }
 
 
-#tree.functional.
+tree.functional.classifyI.leaf <- function(lf,x) {
+    return(nd...cls)
+}
+
+tree.functional.classifyI.node <- function(nd,x) {
+    if(x...attr < nd...attr) {
+        return(tree.functional.classifyI(nd...l, x))
+    } else {
+        return(tree.functional.classifyI(nd...r, x))
+    }
+}
 
 
 mkLeaf <- function(xs, ys, cl) {
---
+++ src/main.r
@@ -0,0 +1,7 @@
+
+
+indians.dat <- read.csv("../data/pima.txt")
+indians.dat.xs <- indians.dat[,1:8]
+indians.dat.ys <- indians.dat[,9]
+
+
