bf9531083774efa9405756167d79380800c73fad
Marco Vassena <m.vassena@students.uu.nl>
2014-09-12 16:12:28 +0200

    Ties broken at random in majority vote

--- src/common.r
+++ src/common.r
@@ -21,27 +21,27 @@ impurity_reduction <- function (s, x, y, i = gini_index) {
   return (delta)
 }
 
-read_data <- function(f) {
-    r.dat <- read.csv("../data/pima.txt")
+read_data <- function(fileName) {
+    r.dat <- read.csv(fileName)
     r.dat.xs <- r.dat[,1:8]
     r.dat.ys <- r.dat[,9]
     return(list(dat=r.dat,xs=r.dat.xs,ys=r.dat.ys))
 }
 
-gini_index <- function(ys) {
-    n1 <- sum(ys)
-    n <- NROW(ys)
+gini_index <- function(y) {
+    n1 <- sum(y)
+    n <- NROW(y)
     return((n1/n) * (1 - (n1/n)))
 }
 
-majority_class <- function(ys) {
-    n1 <- sum(ys)
-    n <- NROW(ys)
-    if (n1 * 2 < n) {
-        return(0)
-    } else {
-        return(1)
-    }
+majority_class <- function(y) {
+    n1 <- sum(y)
+    n <- NROW(y)
+    if (n1 * 2 > n)
+      return(1)
+    if (n1 * 2 < n)
+      return(0)
+    return (sample(0:1,1))
 }
 
 # Function: bestsplit
@@ -58,7 +58,7 @@ majority_class <- function(ys) {
 #   the split that minimizes the impurity function.
 #   If the vectors have length less than 2, NULL is returned.
 #
-bestsplit <- function (x, y){
+best_split <- function (x, y){
   xy = data.frame(x,y)
   sorted <- xy[order(xy$x), ] # TODO should I filter out duplicates?
   x <- sorted$x
