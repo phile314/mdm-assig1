61aeb30af0060daa5366127befef9a03d1e4049e
Marco Vassena <m.vassena@students.uu.nl>
2014-09-18 16:37:13 +0200

    [FIX] Crash for one attribute data as x
      Keep track of split and column split in the tree data structure

--- src/ctree.r
+++ src/ctree.r
@@ -4,10 +4,12 @@ tree.grow <- function(x, y, nmin, minleaf, impurity = gini_index){
   N <- length(y)
   tree <- data.frame(left = rep(NA,N),
                      right = rep(NA, N),
-                     label = rep(NA,N)) # logical? (binary classification)
+                     label = rep(NA,N), # logical? (binary classification)
+                     split = rep(NA, N),
+                     splitCol = rep(NA, N))
 
   # Initialization
-  tree[1, ] <- c(NA, NA, majority_class(y))
+  tree[1, ] <- c(NA, NA, majority_class(y), NA, NA)
   worklist <- list(1)
   samples = list()
   samples[[1]] = 1:length(y)
@@ -18,7 +20,7 @@ tree.grow <- function(x, y, nmin, minleaf, impurity = gini_index){
     worklist[[1]] <- NULL
     current.samples <- samples[[current.index]]
     y.current <- y[current.samples]
-    x.current <- x[current.samples,]
+    x.current <- x[current.samples, , drop = FALSE]
     if(impurity(y.current) > 0) { # || length(samples[current]) < nmin ){
       best <- best_of_best(x.current, y.current)
       
@@ -28,13 +30,13 @@ tree.grow <- function(x, y, nmin, minleaf, impurity = gini_index){
       # Make leaves
       left <- best$nodes$left
       right <- best$nodes$right
-      tree[freeRow,] <- c(NA, NA, majority_class(left$y))
-      tree[freeRow + 1,] <- c(NA, NA, majority_class(right$y)) # TODO should we always enforce two different class labels?! 
+      tree[freeRow,] <- c(NA, NA, majority_class(left$y), NA, NA)
+      tree[freeRow + 1,] <- c(NA, NA, majority_class(right$y), NA, NA) # TODO should we always enforce two different class labels?! 
       samples[[freeRow]] <- current.samples[x.current[, best$col] <= best$split]
       samples[[freeRow + 1]] <- current.samples[(x.current[, best$col] > best$split)]
 
       # Add children to current node
-      tree[current.index, ] <- c(freeRow, freeRow + 1, NA)
+      tree[current.index, ] <- c(freeRow, freeRow + 1, NA, best$split, best$col)
       
       worklist <- c(worklist, freeRow, freeRow + 1)
       freeRow = freeRow + 2      
