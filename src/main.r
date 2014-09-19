ae1236c52d4323794f179c8203c2705463c8b2bd
Philipp Hausmann <ph_git@314.ch>
2014-09-19 17:42:07 +0200

    Fix compile errors

--- src/main.r
+++ src/main.r
@@ -3,9 +3,9 @@ source("common.r")
 source("ctree.r")
 
 test.pima <- function(){
-  pima <- read_data("../data/pima.txt")
-  pima.tree <- tree.grow(pima$xs, pima$ys, nmin = 20, minleaf = 5)
-  pima.actual <- tree.classify(pima$xs, pima.tree)
-  pima.expected <- pima$ys
+  pima <- read_data("../data/pima.txt", 0)
+  pima.tree <- tree.grow(pima$trxs, pima$trys, nmin = 20, minleaf = 5)
+  pima.actual <- tree.classify(pima$trxs, pima.tree)
+  pima.expected <- pima$trys
   return(table(pima.expected, pima.actual))
 }
