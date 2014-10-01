ce90a3e6c69f08a8d17ef9ba32ac25f1cfe2dd9e
Marco Vassena <m.vassena@students.uu.nl>
2014-10-01 18:19:17 +0200

    Added documentation to common

--- src/common.r
+++ src/common.r
@@ -1,5 +1,20 @@
-library('parallel')
-
+# Function: split(s, x, y)
+# Splits the vectors x and y according to the value of s.
+#
+# Arguments
+#   s : A number representing the threshold value to be used to apply the split  
+#   x : A vector containing the values for some binary or numerical attribute
+#   y : A vector containing the respecitve binary class labels for the observations contained in x
+#
+# x and y have the same length.
+# 
+# Result:
+# A list containing the following named fields : left, right, isRight.
+# left and right are also lists containing two named fields (x and y).
+# right$x and right$y are numerical vectors containing the portion of the input
+# vectors x and y for which the x-values are greater than s.
+# right$x and right$y contain the remaining values from x and y.
+# isRight is a logical vector resulting from x > s
 split <- function(s, x, y){
   isRight = x > s
   l = list(x = x[! isRight], y = y[! isRight])
@@ -7,12 +22,25 @@ split <- function(s, x, y){
   return(list(left = l, right = r, isRight = isRight))
 }
 
+# Function: partition(isRight, x, y)
+# Partition the attributes matrix x and the class labels y according to the
+# logical vector isRight.
+#
+# Arguments:
+#   isRight : A logical vector
+#   x       : A numerical matrix
+#   y       : A numerical (binary) vector
+#
+# The number of rows of x and the length of isRight and y are the same.
+#
 # Result
 # A list containing the following named fields:
 #   left.x : left rows of x
 #   right.x : right rows of x
 #   left.y : left rows of y
 #   right.y : right rows of y
+# Where left or right is determined by the logical value of the correspondent
+# element of isRight.
 partition <- function(isRight, x, y) {
   rx = x[isRight, , drop = FALSE]
   lx = x[! isRight, , drop = FALSE]
@@ -21,6 +49,16 @@ partition <- function(isRight, x, y) {
   return(list(left.x = lx, right.x = rx, left.y = ly, right.y = ry))
 }
 
+# Function: reduction(s, x, y, i)
+# Arguments
+#   s : A number representing a treshold value for a split
+#   x : A numerical vector containing values for a binary/numerical attribute 
+#   y : A numerical (binary) vector containing the class labels related to x
+#   i : The impurity function to be used (default = gini_index)
+#
+# The vectors x and y have the same length.
+#
+# Result: A number representing the reduction obtained applying the split s.
 reduction <- function (s, x, y, i = gini_index){
   nodes = split(s, x, y)
   l = nodes$left$y
@@ -30,13 +68,13 @@ reduction <- function (s, x, y, i = gini_index){
   return((i(l) * pl) + (i(r) * pr))
 }
 
-# Function: impurity_reduction
+# Function: impurity_reduction(s, x, y, i)
 #
 # Arguments:
 #   s : A number representing the split on the numerical attributes
 #   x : A vector containing the numerical attributes
 #   y : A vector containing the binary class labels
-#   i : The impurity function used
+#   i : The impurity function used (default = gini_index)
 #   
 # The vectors x and y have the same length.
 #
@@ -75,6 +113,7 @@ read_data <- function(fileName, test, header = FALSE) {
     return(list(dat=r.dat,trxs=r.trxs,trys=r.trys,texs=r.texs,teys=r.teys))
 }
 
+# TODO remove
 read.data <- function(filename){
     r.dat <- read.csv(filename)
     nc <- dim(r.dat)[2]
@@ -83,12 +122,28 @@ read.data <- function(filename){
     return(list(dat=r.dat,xs=r.dat.xs,ys=r.dat.ys))
 }
 
+# Function: gini_index(y)
+# The gini index impurity function for the two-class case
+#
+# Arguments:
+#   y : A binary (numerical) vector, with class labels 0 or 1. 
+#
+# Result: The value of the gini index impurity function for the given class label vector 
 gini_index <- function(y) {
     n1 <- sum(y)
     n <- NROW(y)
     return((n1 / n) * (1 - (n1 / n)))
 }
 
+# majority_class(y)
+# Computes the majority vote for the given vector of class labels.
+#
+# Arguments
+#   y : A binary (numerical) vector, with class labels 0 or 1
+#
+# Result
+#   The class label 0 or 1 which is more frequent in the input vector.
+#   Ties are broken at random.
 majority_class <- function(y) {
     n1 <- sum(y)
     n <- NROW(y)
@@ -99,6 +154,17 @@ majority_class <- function(y) {
     return (sample(0:1, 1))
 }
 
+# Function: candidate_splits(x, y, impurity)
+# Computes the possible splits for the attribute vector x, with class label y and
+# their impurity reduction.
+#
+# Arguments:
+#   x : A numerical vector containing values of a numerical/binary attribute
+#   y : A binary (numerical) vector containing the corresponding class labels for x
+#   impurity : The impurity function to be used when computing the impurity reduction.
+#
+# Result:
+#   A 2-columns matrix containing all the possible splits on x and their impurity reduction.
 candidate_splits <- function(x, y, impurity = gini_index){
   xy <- data.frame(x,y)
   sorted <- xy[order(xy$x), ]
@@ -113,10 +179,25 @@ candidate_splits <- function(x, y, impurity = gini_index){
   return(candidates)
 }
 
+# Function: is_good_split(nodes, minleaf)
+# Determine whether a split meet the minleaf constraint.
+#
+# Arguments:
+#   nodes : A list obtained by the split function
+#   minleaf : An integer number
+#
+# Result: TRUE if both the leaves contained in nodes contain minleaf observations,
+#         FALSE otherwise.
 is_good_split <- function (nodes, minleaf) {
   length(nodes$left$x) >= minleaf && length(nodes$right$x) >= minleaf
 }
 
+# Function: best.split(s1, s2)
+# Returns the best of the two splits given.
+# 
+# Arguments 
+#   s1 s2 : A split object representing a split
+#           The comparison is done on the reduction field.
 best.split <-function(s1, s2){
   if (!"reduction" %in% names(s1))
     return(s2)
@@ -126,9 +207,12 @@ best.split <-function(s1, s2){
   return(best)
 }
 
-# Input a list of split objects.
+# Function: best.split.among(splits)
+# 
+# Arguments:
+#   splits: A list of split objects.
 # Output 
-#   the split in the list with greater reduction, or NULL
+#   The split in the list with greater reduction, or NULL
 #   if the list is empty.
 #   The returned split is extended with a new field index containing its
 #   position in the list.
@@ -141,26 +225,20 @@ best.split.among <- function(splits){
   return(best)
 }
 
-# TODO pass around implicity arguments
-
-# Function: best_split
+# Function: best.split.on(x, y, minleaf, impurity)
+# Computes the best possible split on the attribute vector x.
 #
 # Arguments
-#   x : Vector containing numerical values
+#   x : Vector containing numerical/binary values
 #   y : Vector containing binary class labels
-#   minleaf : The minimum number of observations required to consider a split acceptable
+#   minleaf : The minimum number of observations required to consider a split acceptable (defaut = 0)
+#   impurity : The impurity function that will be used for computing the impurity reduction (default = gini_index)
 #
 # The two vectors have the same length.
-# This version adopts the brute version approach.
 #
 # Result
-#   A list containing three elements:
-#     reduction : The impurity reduction produced by the returned split
-#     split : The numerical value that separates the observations
-#     isRight : A boolean vector, indicating for each row if
-#               it belongs to the right subtree or not.
+#   A split object representing the best possible split on x
 #   NULL if no possible split satisfy the minleaf constraint.
-#
 best.split.on <- function (x, y, minleaf = 0, impurity = gini_index){
   cs <- as.data.frame(candidate_splits(x, y, impurity))
   colnames(cs) <- c('split', 'reduction')
