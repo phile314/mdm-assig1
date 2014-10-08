1f483672cdeb0192fd0c14b5fa59166f98495208
Marco Vassena <m.vassena@students.uu.nl>
2014-10-08 18:36:07 +0200

    Fixed various issues with the benchmarking script

--- src/benchmark.r
+++ src/benchmark.r
@@ -12,7 +12,7 @@ source("functional.r")
 ################################################################################
 
 benchmark <- function(){
-  pima <- read.data('../data/pima.txt', 0)
+  pima <- read.data('../data/pima.txt', 0.3)
   bench.grow <- microbenchmark(tree.grow(pima$train.x, pima$train.y, 20, 5),
                                tree.functional.grow(pima$train.x, pima$train.y, 20, 5),
                                times = 5)
@@ -20,8 +20,8 @@ benchmark <- function(){
   imp.tree <- tree.grow(pima$train.x, pima$train.y, 20, 5)
   fun.tree <- tree.functional.grow(pima$train.x, pima$train.y, 20, 5)
 
-  bench.classify <- microbenchmark(tree.classify(pima$test.y, imp.tree),
-                                   tree.functional.classify(pima$test.y, fun.tree),
+  bench.classify <- microbenchmark(tree.classify(pima$test.x, imp.tree),
+                                   tree.functional.classify(pima$test.x, fun.tree),
                                    times = 10)
   return(list(grow = bench.grow, classify = bench.classify))
 }
@@ -31,8 +31,8 @@ benchmark <- function(){
 ################################################################################
 
 # Computes the error rate for a number of combinations of nmin and minleaf parameters
-search <- function(){
-  data <- read_data('../data/spambase.data', 0.7)
+search.params <- function(){
+  data <- read.data('../data/spambase.data', 0.3)
   nmins <- c(0,1,2,3,4,5,6,8,10,15,20,30,50,80,110,140,170,200,275,400,500,750,1000,1500,3000)
   minleafs <- c(0,1,2,3,4,5,6,8,10,15, 25, 50, 100, 200, 500)
   ps_pars <- list()
@@ -110,3 +110,26 @@ eval_to_df <- function(eres) {
   }
   return(df)
 }
+
+# Function: eval_with_pars
+#
+# Arguments:
+#   data: The dataset on which to evaluate the algorithm.
+#   par : A list of the parameters to use (nmin, minleaf)
+#
+# Result
+# A list of
+#   model : the fitted tree
+#   pred  : the predicted class label
+#   error : the error rate
+#   cm    : the confusion matrix
+#
+# Evaluates the tree classification algorithm.
+eval_with_pars <- function(data, par) {
+  model <- tree.functional.grow(data$train.x, data$train.y, par$nmin, par$minleaf)
+  prys <- tree.functional.classify(data$test.x, model)
+  cm <- cm(prys, data$test.y)
+  er <- error_rate(cm)
+  return(list(model=model, pred=prys, error=er, cm=cm))
+}
+
